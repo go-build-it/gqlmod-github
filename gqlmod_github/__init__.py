27238f89027f51de0d021e694c3fe8e6e0d4762c
Jamie Bliss <jamie@ivyleav.es>
2019-09-03 16:59:27 -0400

    Add some implementations for future extensions.

--- gqlmod_github/__init__.py
+++ gqlmod_github/__init__.py
@@ -1,6 +1,9 @@
 """
 Provider for GitHub's v4 GraphQL API.
 """
+import importlib.resources
+import urllib.request
+
 from gqlmod.helpers.urllib import UrllibJsonProvider
 
 
@@ -13,3 +16,9 @@ class GitHubProvider(UrllibJsonProvider):
     def modify_request(self, req):
         if self.token:
             req.add_header('Authorization', f"Bearer {self.token}")
+
+    def get_schema_str(self):
+        return importlib.resources.read_text(__name__, 'schema.graphql')
+
+    def codegen_extra_kwargs(self, gast, schema):
+        return {}
