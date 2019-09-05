3186bcc045de22afd6a69c0784442c733db1d814
Jamie Bliss <jamie@ivyleav.es>
2019-09-04 22:28:36 -0400

    Add kwarg for previews in use

--- gqlmod_github/__init__.py
+++ gqlmod_github/__init__.py
@@ -1,9 +1,28 @@
 """
 Provider for GitHub's v4 GraphQL API.
 """
+import itertools
 import importlib.resources
 
+import graphql
+
 from gqlmod.helpers.urllib import UrllibJsonProvider
+from gqlmod.helpers.utils import walk_query, walk_variables, unwrap_type
+
+
+def find_directive(ast_node, name):
+    if ast_node is None:
+        return
+    for d in ast_node.directives:
+        if d.name.value == name:
+            break
+    else:
+        return
+
+    return {
+        arg.name.value: graphql.value_from_ast_untyped(arg.value)
+        for arg in d.arguments
+    }
 
 
 class GitHubProvider(UrllibJsonProvider):
@@ -20,6 +39,19 @@ class GitHubProvider(UrllibJsonProvider):
         return importlib.resources.read_text(__name__, 'schema.graphql')
 
     def codegen_extra_kwargs(self, gast, schema):
+        previews = set()
+        for field in itertools.chain(
+            (f for _, _, f in walk_query(gast, schema)),
+            (f for _, f in walk_variables(gast, schema)),
+        ):
+            d = find_directive(field.ast_node, 'preview')
+            if d and 'toggledBy' in d:
+                previews.add(d['toggledBy'])
+
+            typ, *_ = unwrap_type(field.type)
+            d = find_directive(typ.ast_node, 'preview')
+            if d and 'toggledBy' in d:
+                previews.add(d['toggledBy'])
         return {
-            '__spam': 'eggs',
+            '__previews': previews,
         }
